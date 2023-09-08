c1d98543a392e12ab7c5fb6e58805b77d8a1fed9
Mukaddes Uzdoganlar <m_uzdoganlar@web.de>
2023-09-08 12:07:11 +0200

    solution-hello-world

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-function Greeting() {
-  <h1>Hello World!</h1>;
+function HelloWorld() {
+  return <h1>Hello world!</h1>;
 }
 
 root.render(
   <StrictMode>
-    <App />
-    <Greeting />
+    <HelloWorld />
   </StrictMode>
 );
